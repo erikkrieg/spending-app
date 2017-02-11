c030b75fe079d3e1598cb21ee3443a3f9cc2c93d
Erik Krieg <erik.c.krieg@gmail.com>
2017-02-11 17:23:41 -0500

    app now starts listening to port after connecting to db

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 var express = require('express');
+var MongoClient = require('mongodb').MongoClient;
+var mongodbURL = require('./cred.js').mongodb.url;
+
+var db;
 var app = express();
 
 app.use('/', express.static('public'))
@@ -8,6 +12,10 @@ app.get('/api', function (req, res) {
     res.send('Hello World!');
 });
 
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
+MongoClient.connect(mongodbURL, function (err, database) {
+    if (err) return console.log(err);
+    db = database;
+    app.listen(3000, function () {
+        console.log('Example app listening on port 3000!', db.s.databaseName);
+    });
 });
