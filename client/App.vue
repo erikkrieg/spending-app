7b01d3c1953f2756b2f64ba35e8334c0e470a1d0
Erik Krieg <erik.c.krieg@gmail.com>
2017-02-12 16:44:43 -0500

    adds button to trigger opening of add purchase form

--- client/App.vue
+++ client/App.vue
@@ -1,6 +1,10 @@
 <template>
     <div id="app">
-        <app-header></app-header>
+        <app-header />
+        <app-purchase-date-range />
+        <app-purchase-chart type="pie" />
+        <app-purchase-table />
+        <button class="app__add-purchase-btn" v-on:click="openAddPurchase"><span>+<span></button>
     </div>
 </template>
 
@@ -15,6 +19,11 @@
         },
         components: {
             AppHeader
+        },
+        methods: {
+            openAddPurchase () {
+                window.alert('create add-purchase form component');
+            }
         }
     };
 </script>
@@ -26,5 +35,26 @@
         -moz-osx-font-smoothing: grayscale;
         text-align: center;
         color: #2c3e50;
+    }
+
+    .app__add-purchase-btn {
+        position: fixed;
+        bottom: 20px;
+        right: 20px;
+        width: 40px;
+        height: 40px;
+        border: none;
+        border-radius: 50%;
+        outline: none;
+        background-color: #E61875;
+        color: white;
+        cursor: pointer;
+
+        span {
+            position: relative;
+            top: -4px;
+            font-size: 2rem;
+            pointer-events: none;
+        }
     }
 </style>
