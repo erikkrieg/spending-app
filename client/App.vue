6bf18bb9f0c204aa1a3511431895d935908f4766
Erik Krieg <erik.c.krieg@gmail.com>
2017-02-12 15:56:46 -0500

    adds app header component to app

--- client/App.vue
+++ client/App.vue
@@ -1,19 +1,20 @@
 <template>
     <div id="app">
-        <header>
-            <h1>Spent</h1>
-            <a href="logout">logout</a>
-        </header>
+        <app-header></app-header>
     </div>
 </template>
 
 <script>
+    import AppHeader from './components/AppHeader.vue';
     export default {
         name: 'app',
         data () {
             return {
                 msg: 'Welcome to Your Vue.js App'
             };
+        },
+        components: {
+            AppHeader
         }
     };
 </script>
@@ -25,17 +26,5 @@
         -moz-osx-font-smoothing: grayscale;
         text-align: center;
         color: #2c3e50;
-    }
-
-    header {
-        display: flex;
-        align-items: center;
-        > * {
-            margin: 10px;
-        }
-    }
-
-    h1 {
-        font-weight: normal;
     }
 </style>
