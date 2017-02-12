28a8c1dfbd7f27247b2151c63ba004d546f9583e
Erik Krieg <erik.c.krieg@gmail.com>
2017-02-12 18:18:50 -0500

    fixes linter errors

--- server/models/user.js
+++ server/models/user.js
@@ -1,19 +1,19 @@
-var mongoose = require('mongoose');  
+var mongoose = require('mongoose');
 var bcrypt = require('bcrypt-nodejs');
 
-var userSchema = mongoose.Schema({  
+var userSchema = mongoose.Schema({
     local: {
         email: String,
-        password: String,
+        password: String
     }
 });
 
-userSchema.methods.generateHash = function (password) {  
+userSchema.methods.generateHash = function (password) {
     return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
 };
 
-userSchema.methods.validPassword = function(password) {  
+userSchema.methods.validPassword = function (password) {
     return bcrypt.compareSync(password, this.local.password);
 };
 
-module.exports = mongoose.model('User', userSchema); 
+module.exports = mongoose.model('User', userSchema);
