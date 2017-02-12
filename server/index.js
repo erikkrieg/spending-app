94503ac718dac8b45cfeda528f7f4effd12f67f3
Erik Krieg <erik.c.krieg@gmail.com>
2017-02-12 12:53:38 -0500

    fixes linter errors

--- server/index.js
+++ server/index.js
@@ -1,9 +1,9 @@
 var express = require('express');
-var passport = require('passport');  
-var mongoose = require('mongoose'); 
+var passport = require('passport');
+var mongoose = require('mongoose');
 
 var cred = require('./cred.js');
-var routes = require('./routes/index'); 
+var routes = require('./routes/index');
 var configPassport = require('./config/passport');
 
 var app;
@@ -13,23 +13,23 @@ mongoose.connect(cred.mongodb.url);
 
 app = express();
 app.set('view engine', 'ejs');
-app.use(express.static('public'))
+app.use(express.static('public'));
 app.use(require('cookie-parser')());
 app.use(require('body-parser').urlencoded({ extended: true }));
 app.use(require('express-session')({ secret: cred.session.secret, resave: true, saveUninitialized: true }));
-app.use(passport.initialize());  
-app.use(passport.session()); 
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(require('connect-flash')());
-app.use('/', routes); 
+app.use('/', routes);
 
 configPassport(passport);
 
-app.use(function (req, res, next) {  
+app.use(function (req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
 });
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
     res.status(err.status || 500);
     res.send(err.message);
 });
