178f424842ee932bc451135416f99291b22daf60
Erik Krieg <erik.c.krieg@gmail.com>
2017-02-12 13:01:04 -0500

    fixes linter errors

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,9 @@
-var express = require('express');  
-var passport = require('passport');  
+var express = require('express');
+var passport = require('passport');
 var router = express.Router();
 
-router.get('/', function (req, res, next) { 
-    console.log('isAuthenticated', req.isAuthenticated())
+router.get('/', function (req, res, next) {
+    console.log('isAuthenticated', req.isAuthenticated());
     if (req.isAuthenticated()) {
         res.render('pages/app', {
             userId: req.user._id,
@@ -14,21 +14,21 @@ router.get('/', function (req, res, next) {
     }
 });
 
-router.get('/logout', function (req, res) {  
+router.get('/logout', function (req, res) {
     req.logout();
     res.redirect('/');
 });
 
-router.post('/signup', passport.authenticate('local-signup', {  
-  successRedirect: '/',
-  failureRedirect: '/',
-  failureFlash: true,
+router.post('/signup', passport.authenticate('local-signup', {
+    successRedirect: '/',
+    failureRedirect: '/',
+    failureFlash: true
 }));
 
-router.post('/login', passport.authenticate('local-login', {  
-  successRedirect: '/',
-  failureRedirect: '/',
-  failureFlash: true,
+router.post('/login', passport.authenticate('local-login', {
+    successRedirect: '/',
+    failureRedirect: '/',
+    failureFlash: true
 }));
 
 module.exports = router;
